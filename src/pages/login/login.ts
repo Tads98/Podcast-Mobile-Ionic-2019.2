6216dfe89fd7a1f349e3001e344953c187f693e0
Thales <thales.a@academico.ifrn.edu.br>
2019-09-26 16:06:59 -0300

    Incluindo tela de login do usuário

---
+++ src/pages/login/login.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {NavController} from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { TabsPage } from '../tabs/tabs';
+import { IntroPage } from '../intro/intro';
+
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController) {    
+  }
+
+  goToTabsPage(){
+    this.navCtrl.push(HomePage)
+  }
+
+
+}
